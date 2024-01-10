4be577525c4f62eee89fac31411adee8895a721c
Annt <annguyenthe.work@gmail.com>
2024-01-10 23:33:19 +0700

    add api get account

--- api/account.go
+++ api/account.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"database/sql"
 	"net/http"
 
 	db "github.com/anthanh17/simplebank/db/sqlc"
@@ -8,28 +9,54 @@ import (
 )
 
 type createAccountRequest struct {
-	Owner 	 string `json:"owner" binding:"required"`
-	Currency string `json:"currency" binding:"required, oneof=USD EUR"`
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required,oneof=USD EUR"`
 }
 
 func (server *Server) createAccount(ctx *gin.Context) {
 	var req createAccountRequest
 
-	// validate input data
 	if err := ctx.ShouldBindJSON(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
 
 	// create account
-	arg := db.CreateAccountParams {
-		Owner: req.Owner,
+	arg := db.CreateAccountParams{
+		Owner:    req.Owner,
 		Currency: req.Currency,
-		Balance: 0,
+		Balance:  0,
 	}
 
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	// retunr account
+	ctx.JSON(http.StatusOK, account)
+}
+
+type getAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getAccount(ctx *gin.Context) {
+	var req getAccountRequest
+
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	account, err := server.store.GetAccount(ctx, req.ID)
+	if err != nil {
+		// Case: the account with the specific input ID doesn't exist
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
--- api/server.go
+++ api/server.go
@@ -7,17 +7,18 @@ import (
 
 // Server serves HTTP requests for our banking service.
 type Server struct {
-	store *db.Store
+	store  *db.Store
 	router *gin.Engine
 }
 
 // NewServer creates a new HTTP server and setup routing.
-func NewServer(store *db.Store) * Server {
+func NewServer(store *db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
 	// add routes to router
 	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
 
 	server.router = router
 	return server
