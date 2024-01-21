a13393f8ac98208285fd837eb80b4622675e971f
Annt <annguyenthe.work@gmail.com>
2024-01-21 22:26:13 +0700

    implement login user API with PASETo

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 	_ "github.com/lib/pq"
 )
 
-
 func main() {
 	config, err := util.LoadConfig(".")
 	if err != nil {
@@ -23,7 +22,10 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
-	server := api.NewServer(store)
+	server, err := api.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server:", err)
+	}
 
 	err = server.Start(config.ServerAddress)
 	if err != nil {
